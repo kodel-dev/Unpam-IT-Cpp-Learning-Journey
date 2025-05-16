a5d920a0341a0d340b3364219ba6204e7a578915
Kodel <198582294+kodel-dev@users.noreply.github.com>
2025-05-16 10:19:05 +0700

    Create variabel_dan_tipe_data.cpp

---
+++ 01-Pemrograman-Dasar/variabel_dan_tipe_data.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int umur = 20;
+    float ipk = 3.75;
+    char nilai = 'A';
+    string nama = "Muhammad Amin";
+
+    cout << "Nama: " << nama << endl;
+    cout << "Umur: " << umur << endl;
+    cout << "IPK: " << ipk << endl;
+    cout << "Nilai: " << nilai << endl;
+    return 0;
+}
