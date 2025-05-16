559a602b1360547472dadaaf91502ff72cf288fe
Kodel <198582294+kodel-dev@users.noreply.github.com>
2025-05-16 10:28:03 +0700

    Create array.cpp

---
+++ 02-Function-dan-Array/array.cpp
@@ -0,0 +1,8 @@
+using namespace std;
+int main() {
+    int nilai[3] = {80, 85, 90};
+    for (int i = 0; i < 3; i++) {
+        cout << "Nilai ke-" << i + 1 << ": " << nilai[i] << endl;
+    }
+    return 0;
+}
